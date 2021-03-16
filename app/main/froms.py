1fc8df7d73f58d8f4fd69182be79985fe1ec43ae
Salo <salomonco@gmail.com>
2021-03-16 16:46:31 -0300

    Join Party Form created

--- app/main/froms.py
+++ app/main/froms.py
@@ -12,3 +12,19 @@ class CreatePartyForm(FlaskForm):
     '''Form for creating a party'''
     name = StringField("Party Name", validators=[DataRequired(), Length(min=3, max=80)])
     submit = SubmitField('Create Party!')
+
+
+# Join Party Form
+
+class JoinPartyForm(FlaskForm):
+    '''Form for joining a party. It checks the special code 
+    to ensure the user is joining an existing party'''
+    code = StringField("Code", validators=[
+                       DataRequired(), Length(min=3, max=80)])
+    submit = SubmitField('Submit!')
+
+    def validate_code(self, code):
+        party = Party.query.filter_by(code=code.data).first()
+        if not party:
+            raise ValidationError(
+                'That code is incorrect. Please try again.')
