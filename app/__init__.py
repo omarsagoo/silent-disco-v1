654adfaa8989fad61312c71b0897506a007898e1
Salo <salomonco@gmail.com>
2021-03-16 18:47:48 -0300

    added authorization code

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from .models import User
 import os
 
 from flask import Flask
@@ -5,6 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 
 from app.config import Config
+from flask_login import LoginManager, login_manager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,7 +21,17 @@ db = SQLAlchemy(app)
 ###########################
 # Authentication
 ###########################
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+bcrypt = Bcrypt(app)
 
 ###########################
 # Blueprints
