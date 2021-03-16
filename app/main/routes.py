b3ece92e69ee3089edc7eadfffc0e8a18cd3cb0b
omarsagoo <om.sagoo@gmail.com>
2021-03-15 21:54:18 -0700

    flask starter code, update

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
+
+@main.route('/')
+def homepage():
+    return render_template('main/home.html')
