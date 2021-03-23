9f6abb3ec59660f0b4d4125d0c287719b884eeaf
Salo <salomonco@gmail.com>
2021-03-23 13:40:34 -0300

    [ADD] setup code

--- app/auth/tests.py
+++ app/auth/tests.py
@@ -4,3 +4,29 @@ from unittest import TestCase
 from datetime import date
 from app import app, db, bcrypt
 from app.models import User
+
+
+# Helper Functions
+
+def create_user():
+    password_hash = bcrypt.generate_password_hash('password').decode('utf-8')
+    user = User(username='me1', password=password_hash, name='test')
+    db.session.add(user)
+    db.session.commit()
+
+    
+# Tests
+
+
+class AuthTests(TestCase):
+    """Tests for authentication (login & signup)."""
+
+    def setUp(self):
+        """Executed prior to each test."""
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
